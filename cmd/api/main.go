0af520ba59b19ed41d4d523737c2cc754fb4afa4
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-01-16 21:32:26 +0100

    17.3 Simple CORS Requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -68,6 +72,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "username", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "password", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.pp.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
