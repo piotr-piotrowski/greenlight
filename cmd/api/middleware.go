ca89a4eb4c6cdc54e7d554bdf8d5c8d6071b3049
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-01-18 18:05:10 +0100

    18.4 Recording HTTP Status Codes

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -203,11 +204,48 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	})
 }
 
+type metricsResponseWriter struct {
+	wrapped       http.ResponseWriter
+	statusCode    int
+	headerWritten bool
+}
+
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped:    w,
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) Unwrap() http.ResponseWriter {
+	return mw.wrapped
+}
+
 func (app *application) metrics(next http.Handler) http.Handler {
 	var (
 		totalRequestReceived            = expvar.NewInt("total_requests_received")
 		totalResponseSent               = expvar.NewInt("total_responses_send")
 		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_μs")
+
+		totalResponsesSentByStatus = expvar.NewMap("total_responses_sent_by_status")
 	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -215,9 +253,13 @@ func (app *application) metrics(next http.Handler) http.Handler {
 
 		totalRequestReceived.Add(1)
 
-		next.ServeHTTP(w, r)
+		mw := newMetricsResponseWriter(w)
+
+		next.ServeHTTP(mw, r)
 
 		totalResponseSent.Add(1)
+
+		totalResponsesSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
 
 		duration := time.Since(start).Microseconds()
 		totalProcessingTimeMicroseconds.Add(duration)
